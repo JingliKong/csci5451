2d30987acc85756f255cb2483641eb8106e782e4
Angie Chen <chen7313@umn.edu>
2023-04-05 23:13:08 -0500

    edits

--- a2-code/heat_mpi.c
+++ a2-code/heat_mpi.c
@@ -2,6 +2,16 @@
 #include <mpi.h>
 #include <stdlib.h>
 
+// // #define MGET(mat, i, j) ((mat)->data[((i)*((mat)->ncols)) + (j)])
+// // #define MSET(mat, i, j, x) ((mat)->data[((i)*((mat)->ncols)) + (j)] = (x))
+// #define HGET(mat, i, j) (mat[i * (width) + j])
+// #define HSET()
+// // #define LGET()
+// // #define LSET()
+#define PROC_GET(proc_data, cols_per_proc, i, j) ((proc_data)[(i * cols_per_proc) + (j)])
+#define PROC_SET(proc_data, cols_per_proc, i, j, x) (((proc_data)[(i * cols_per_proc) + (j)]) = (x))
+
+
 int main(int argc, char **argv){
     if(argc < 4){
         printf("usage: %s max_time width print\n", argv[0]);
@@ -18,7 +28,8 @@ int main(int argc, char **argv){
     double L_bound_temp = 20.0;   // Constant temp at Left end of rod
     double R_bound_temp = 10.0;   // Constant temp at Right end of rod
     double k = 0.5;               // thermal conductivity constant
-    double **H;                   // 2D array of temps at times/locations
+    // double **H;                   // 2D array of temps at times/locations
+    double* H;
 
     //Begin MPI code:
     MPI_Init (&argc, &argv); 
@@ -38,120 +49,137 @@ int main(int argc, char **argv){
         printf("Not emough columns per proc\n");
         return 0;
     }
-    // Allocate space for local data
-    double** proc_data = malloc(sizeof(double*) * max_time);
-    for (int i = 0; i < max_time; i++){
-        proc_data[i] = malloc(sizeof(double) * cols_per_proc);
-    }
     
-    volatile int t = 0; //initalize t to 0
-    volatile int p;
+    // Allocate space for local data
+    double* proc_data = calloc(max_time * cols_per_proc, sizeof(double));
+    
 
     if (proc_id == 0) {
         // Allocate memory for collective data
-        H = malloc(sizeof(double*)*max_time); 
-        for(t = 0; t < max_time; t++){
-            // H[t] = malloc(sizeof(double)*width);
-            H[t] = calloc(width, sizeof(double)); 
-        }
+        // H = malloc(sizeof(double*)*max_time); 
+        H = malloc(sizeof(double*)*max_time*width);
+        // for(int t = 0; t < max_time; t++){
+        //     // H[t] = malloc(sizeof(double)*width);
+        //     H[t] = calloc(width, sizeof(double)); 
+        // }
 
-        // Initialize constant left/right boundary temperatures
-        for(t=0; t < max_time; t++){
-            H[t][0] = L_bound_temp;
-            H[t][width-1] = R_bound_temp;
-        }
-        // Initialize temperatures at time 0
-        t=0;
-        for(p=1; p < width-1; p++){
-            H[t][p] = initial_temp;
-        }
+        // // Initialize constant left/right boundary temperatures
+        // for(int t=0; t < max_time; t++){
+        //     H[t][0] = L_bound_temp;
+        //     H[t][width-1] = R_bound_temp;
+        // }
+        // // Initialize temperatures at time 0
+        // for(int p=1; p < width-1; p++){
+        //     H[0][p] = initial_temp;
+        // }
 
         // Proc 0 initializes partition of H to starting conditions 
-        proc_data[t][0] = L_bound_temp; 
         for (int i = 1; i < cols_per_proc; i++) {
-            proc_data[t][i] = initial_temp; 
+            PROC_SET(proc_data, cols_per_proc, 0, i, initial_temp);
         }
         // Initialize constant left boundary temperatures
-        for(t=0; t < max_time; t++){
-            proc_data[t][0] = L_bound_temp;
+        for(int t=0; t < max_time; t++){
+            PROC_SET(proc_data, cols_per_proc, t, 0, L_bound_temp); 
         }
+
     }
 
     if (proc_id == total_procs - 1) { // we are the leftmost portion of the pipe 
-        // proc_data[t][cols_per_proc - 1] = R_bound_temp; 
-        t = 0;
-        for (int i = 1; i < cols_per_proc; i++) {
-            proc_data[t][i] = initial_temp; 
-        }
         // Proc n-1 initializes partition of H to starting conditions 
-        proc_data[t][0] = L_bound_temp; 
-        int i = (total_procs == 1) ? 0 : 1;
+        int i = (total_procs == 1) ? 1 : 0;
         for (; i < cols_per_proc - 1; i++) {
-            proc_data[t][i] = initial_temp; 
+            PROC_SET(proc_data, cols_per_proc, 0, i, initial_temp);
+        }
+        // Initialize constant right boundary temperatures
+        for(int t=0; t < max_time; t++){
+            PROC_SET(proc_data, cols_per_proc, t, cols_per_proc-1, R_bound_temp);
         }
 
-        // Initialize constant right boundary temperatures
-        for(t=0; t < max_time; t++){
-            proc_data[t][cols_per_proc-1] = R_bound_temp;
-        }
-    } else { // we are in the middle all the initial temps are just 0 
-        for (int i = 0; i < cols_per_proc; i++) {
-            proc_data[t][i] = initial_temp; 
-        }
     }
 
-    // Simulate the temperature changes for internal cells
-    for(t = 0; t < max_time-1; t++){
-        for(p = 1; p < cols_per_proc - 1; p++){
-            double left_diff  = proc_data[t][p] - proc_data[t][p-1];
-            double right_diff = proc_data[t][p] - proc_data[t][p+1];
-            double delta = -k*( left_diff + right_diff );
-            proc_data[t+1][p] = proc_data[t][p] + delta;
+    if ((proc_id != 0) && (proc_id != total_procs - 1)) { // we are in the middle all the initial temps are just 0 
+        for (int i = 0; i < cols_per_proc; i++) {
+            PROC_SET(proc_data, cols_per_proc, 0, i, initial_temp);
         }
+    }
+    
+    // Simulate the temperature changes for internal cells
+    for(int t = 0; t < max_time-1; t++){
+        for(int p = 1; p < cols_per_proc - 1; p++){
+            // printf("i: %d, LEFTVAL: %f, RIGHTVAL: %f\n", p, PROC_GET(proc_data, cols_per_proc, t, p - 1), PROC_GET(proc_data, cols_per_proc, t, p + 1));
+            double prev = PROC_GET(proc_data, cols_per_proc, t, p);
+            // printf("PREV: %f\n", prev);
+
+            // printf("t:%d ", t);
+            double left_diff = prev - PROC_GET(proc_data, cols_per_proc, t, p - 1);
+            double right_diff = prev - PROC_GET(proc_data, cols_per_proc, t, p + 1);
+            double delta = -k*( left_diff + right_diff);
+            // printf("E: %f, A: %f", prev, proc_data[t][p]); 
+            
+            // printf(" %5.1f:",prev);
+
+            // printf(" index:%d \n", (t+1) * cols_per_proc + p);
+            proc_data[(t+1) * cols_per_proc + p] = prev+delta;
+            // PROC_SET(proc_data, cols_per_proc, t+1, p, prev + delta);
+            // PROC_SET(proc_data, cols_per_proc, t+1, p, -123);
+            // printf(":%5.1f ",PROC_GET(proc_data, cols_per_proc, t + 1, p));
+
+
+            
+        }
+        // printf("proc:%d --> t: %d\n", proc_id, t);
+
+        
 
         // Communicate accross processors
         double recv_left, recv_right;
         double send_left, send_right; 
 
         if (total_procs <= 1) {
+            // printf("\n");
+            // printf("should not happen");
+
             continue;
         } else if (proc_id == 0) { // Leftmost processor case
-            send_right = proc_data[t][cols_per_proc - 1];
+            send_right = PROC_GET(proc_data, cols_per_proc, t, cols_per_proc - 1);
             MPI_Sendrecv(&send_right, 1, MPI_DOUBLE, 1, 1,
                           &recv_right, 1, MPI_DOUBLE, 1, 1, 
                           MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-            recv_left = proc_data[t][cols_per_proc - 2];
-            proc_data[t+1][cols_per_proc - 1] = proc_data[t][cols_per_proc - 1] + -k*(2*proc_data[t][cols_per_proc - 1] - recv_left - recv_right);
+            double left = PROC_GET(proc_data, cols_per_proc, t, cols_per_proc - 2);
+            double delta = -k*(2*send_right - left - recv_right);
+            // PROC_SET(proc_data, cols_per_proc, t+1, cols_per_proc-1, send_right + delta);
+            proc_data[(t+1) * cols_per_proc + cols_per_proc-1] = send_right+delta;
     
         } else if ((proc_id == (total_procs - 1))) { // Rightmost processor case
-            send_left = proc_data[t][0];
+            send_left = PROC_GET(proc_data, cols_per_proc, t, 0);
             MPI_Sendrecv(&send_left, 1, MPI_DOUBLE, proc_id - 1, 1,
                           &recv_left, 1, MPI_DOUBLE, proc_id - 1, 1, 
                           MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-            recv_right = proc_data[t][1];              
-            proc_data[t+1][0] = proc_data[t][0] + -k*(2*proc_data[t][0] - recv_left - recv_right);          
+            double right = PROC_GET(proc_data, cols_per_proc, t, 1); 
+            double delta = -k*(2*send_left - recv_left - right);
+            // PROC_SET(proc_data, cols_per_proc, t+1, 0, send_left + delta);
+            proc_data[(t+1) * cols_per_proc] = send_left+delta;          
             
         } else { // Middle processor case 
             // We need 2 MPI_Sendrecv one to the proc to the right and one to the left 
             // sending my right most element to the recv_left because we are sending 
-
             if (proc_id % 2 == 0) {
-                send_right = proc_data[t][cols_per_proc - 1];
+                send_right = PROC_GET(proc_data, cols_per_proc, t, cols_per_proc - 1); 
                 MPI_Sendrecv(&send_right, 1, MPI_DOUBLE, proc_id + 1, 1, 
                             &recv_right, 1, MPI_DOUBLE, proc_id + 1, 1,
                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                send_left = proc_data[t][0];               
+                send_left = PROC_GET(proc_data, cols_per_proc, t, 0);             
                 MPI_Sendrecv(&send_left, 1, MPI_DOUBLE, proc_id - 1, 1,
                             &recv_left, 1, MPI_DOUBLE, proc_id - 1, 1, 
                             MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
             } else {
-                send_left = proc_data[t][0];               
+                send_left = PROC_GET(proc_data, cols_per_proc, t, 0);              
                 MPI_Sendrecv(&send_left, 1, MPI_DOUBLE, proc_id - 1, 1,
                             &recv_left, 1, MPI_DOUBLE, proc_id - 1, 1, 
                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                send_right = proc_data[t][cols_per_proc - 1];
+                send_right = PROC_GET(proc_data, cols_per_proc, t, cols_per_proc - 1); 
                 MPI_Sendrecv(&send_right, 1, MPI_DOUBLE, proc_id + 1, 1, 
                             &recv_right, 1, MPI_DOUBLE, proc_id + 1, 1,
                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -160,66 +188,99 @@ int main(int argc, char **argv){
 
             // Then we just calculate the ends of our local array using the values we just recieved 
             // calculating the end 
-            double left_diff = proc_data[t][0] - recv_left;  
-            double right_diff = proc_data[t][0] - proc_data[t][1];
+            double left = PROC_GET(proc_data, cols_per_proc, t, 0);
+            double left_diff = left - recv_left;  
+            double right_diff = left - PROC_GET(proc_data, cols_per_proc, t, 1);
             double delta = -k*( left_diff + right_diff );
-            proc_data[t+1][0] = proc_data[t][0] + delta;
+            // PROC_SET(proc_data, cols_per_proc, t+1, 0, send_left + delta); 
+            proc_data[(t+1) * cols_per_proc] = send_left+delta;
+             
 
             // now computing for the rightmost element of local array 
-            left_diff = proc_data[t][0] - proc_data[t][cols_per_proc - 2];
-            right_diff =  proc_data[t][0] - recv_right; 
+            double right = PROC_GET(proc_data, cols_per_proc, t, cols_per_proc - 1);
+            left_diff = right - PROC_GET(proc_data, cols_per_proc, t, cols_per_proc - 2);
+            right_diff =  right - recv_right; 
             delta = -k*( left_diff + right_diff );
-            proc_data[t+1][cols_per_proc - 1] = proc_data[t][cols_per_proc] + delta;
+            PROC_SET(proc_data, cols_per_proc, t+1, cols_per_proc - 1, send_right + delta);  
+            proc_data[(t+1) * cols_per_proc + (cols_per_proc-1)] = send_right+delta;
         }
     }
 
-    // Gather all of the results
-    MPI_Gather(proc_data, cols_per_proc, MPI_DOUBLE,
-                H, cols_per_proc, MPI_DOUBLE,
-                root_processor, MPI_COMM_WORLD); 
+    for (int i = 0; i < max_time; i++){
+        // printf("addra: %x\n", proc_data);
+        // printf("addrb: %x\n", proc_data + (i*cols_per_proc));
+        //  printf("addrb: %x\n", &proc_data[0]);
+        // printf("access: %x\n", (proc_data + (i*cols_per_proc))[0]);
+        // printf("%d\n", proc_id);
+
+        // MPI_Gather(proc_data + (i*cols_per_proc), cols_per_proc, MPI_DOUBLE,
+        //             H[i], cols_per_proc, MPI_DOUBLE, 
+        //             root_processor, MPI_COMM_WORLD);
+
+        MPI_Gather(&proc_data[i*cols_per_proc], cols_per_proc, MPI_DOUBLE,
+                    &H[i*width], cols_per_proc, MPI_DOUBLE, 
+                    root_processor, MPI_COMM_WORLD);
+    }
+
+    // printf("YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE\n");
 
     // Print the results
-    if(print){
+    // if((proc_id == 0) && print){
+    //     // Column headers
+    //     printf("%3s| ","");
+    //     for(int p=0; p<width; p++){
+    //         printf("%5d ",p);
+    //     }
+    //     printf("\n");
+    //     printf("%3s+-","---");
+    //     for(int p=0; p<width; p++){
+    //         printf("------");
+    //     }
+    //     printf("\n");
+    //         // Row headers and data
+    //         for(int t=0; t<max_time; t++){
+    //             printf("%3d| ",t);
+    //             for(int p=0; p<width; p++){
+    //             printf("%5.1f ",H[t][p]); // check value of p
+    //             }
+    //             printf("\n");
+    //         }
+    // }
+
+    if((proc_id == 0) && print){
         // Column headers
         printf("%3s| ","");
-        for(p=0; p<width; p++){
+        for(int p=0; p<width; p++){
             printf("%5d ",p);
         }
         printf("\n");
         printf("%3s+-","---");
-        for(p=0; p<width; p++){
+        for(int p=0; p<width; p++){
             printf("------");
         }
         printf("\n");
             // Row headers and data
-            for(t=0; t<max_time; t++){
+            for(int t=0; t<max_time; t++){
                 printf("%3d| ",t);
-                for(p=0; p<width; p++){
-                printf("%5.1f ",H[t][p]);
+                for(int p=0; p<width; p++){
+                // printf("%5.1f ",H[t][p]); // check value of p
+                printf("%5.1f ",H[t*width + p]);
                 }
                 printf("\n");
             }
     }
 
 
-    // Clean up and deallocation
-    for(t=0; t<max_time; t++){
-        printf("%x ------ %x\n", H[t], proc_data[t]);
-        if (proc_id == 0) {
-            free(H[t]);
-        }
-            
-        free(proc_data[t]);
-    }
-   
     free(proc_data);
 
-    if (proc_id == 0) {
+    // Clean up and deallocation
+    if (proc_id == 0){
+        // for (int i = 0; i < max_time; i++){
+        //     // free(H[i]);
+        // }
         free(H);
     }
-    
 
     MPI_Finalize();
-
     return 0;
 }
---
+++ a2-code/heat_mpi_old.c
@@ -0,0 +1,234 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+    if(argc < 4){
+        printf("usage: %s max_time width print\n", argv[0]);
+        printf("  max_time: int\n");
+        printf("  width: int\n");
+        printf("  print: 1 print output, 0 no printing\n");
+        return 0;
+    }
+
+    int max_time = atoi(argv[1]); // Number of time steps to simulate
+    int width = atoi(argv[2]);    // Number of cells in the rod
+    int print = atoi(argv[3]);
+    double initial_temp = 50.0;   // Initial temp of internal cells 
+    double L_bound_temp = 20.0;   // Constant temp at Left end of rod
+    double R_bound_temp = 10.0;   // Constant temp at Right end of rod
+    double k = 0.5;               // thermal conductivity constant
+    double **H;                   // 2D array of temps at times/locations
+
+    //Begin MPI code:
+    MPI_Init (&argc, &argv); 
+
+    //MPI variables
+    int proc_id, total_procs, cols_per_proc;
+    MPI_Comm_rank (MPI_COMM_WORLD, &proc_id);     // get current process id 
+    MPI_Comm_size (MPI_COMM_WORLD, &total_procs); // get number of processes 
+    int root_processor = 0; // have the root processor be processor 0
+
+    //Confirm unsupported configurations fail
+    if ((width % total_procs) != 0) {
+        printf("Number of columns not divisible by procs\n");
+        return 0;
+
+    } else if ((cols_per_proc = width / total_procs) < 3) {
+        printf("Not emough columns per proc\n");
+        return 0;
+    }
+    // Allocate space for local data
+    double** proc_data = malloc(sizeof(double*) * max_time);
+    for (int i = 0; i < max_time; i++){
+        proc_data[i] = malloc(sizeof(double) * cols_per_proc);
+    }
+    
+    int t = 0; //initalize t to 0
+    int p;
+
+    if (proc_id == 0) {
+        // Allocate memory for collective data
+        H = malloc(sizeof(double*)*max_time); 
+        for(t = 0; t < max_time; t++){
+            // H[t] = malloc(sizeof(double)*width);
+            H[t] = calloc(width, sizeof(double)); 
+        }
+
+        // Initialize constant left/right boundary temperatures
+        for(t=0; t < max_time; t++){
+            H[t][0] = L_bound_temp;
+            H[t][width-1] = R_bound_temp;
+        }
+        // Initialize temperatures at time 0
+        t=0;
+        for(p=1; p < width-1; p++){
+            H[t][p] = initial_temp;
+        }
+
+        // Proc 0 initializes partition of H to starting conditions 
+        proc_data[t][0] = L_bound_temp; 
+        for (int i = 1; i < cols_per_proc; i++) {
+            proc_data[t][i] = initial_temp; 
+        }
+        // Initialize constant left boundary temperatures
+        for(t=0; t < max_time; t++){
+            proc_data[t][0] = L_bound_temp;
+        }
+
+    }
+
+    if (proc_id == total_procs - 1) { // we are the leftmost portion of the pipe 
+        t = 0;
+        // Proc n-1 initializes partition of H to starting conditions 
+        proc_data[t][0] = L_bound_temp; 
+        int i = (total_procs == 1) ? 1 : 0;
+        for (; i < cols_per_proc - 1; i++) {
+            proc_data[t][i] = initial_temp; 
+        }
+        // Initialize constant right boundary temperatures
+        for(t=0; t < max_time; t++){
+            proc_data[t][cols_per_proc-1] = R_bound_temp;
+        }
+
+    }
+
+    if ((proc_id != 0) && (proc_id != total_procs - 1)) { // we are in the middle all the initial temps are just 0 
+        for (int i = 0; i < cols_per_proc; i++) {
+            proc_data[t][i] = initial_temp; 
+        }
+    }
+    
+    // Simulate the temperature changes for internal cells
+    for(t = 0; t < max_time-1; t++){
+
+        for(p = 1; p < cols_per_proc - 1; p++){
+            // printf("tp: %d %d --->", t, p);
+            // printf("lr - %f %f\n", proc_data[t][p-1], proc_data[t][p+1]);
+            double left_diff  = proc_data[t][p] - proc_data[t][p-1];
+            double right_diff = proc_data[t][p] - proc_data[t][p+1];
+            double delta = -k*( left_diff + right_diff );
+            proc_data[t+1][p] = proc_data[t][p] + delta;
+        }
+
+        // printf("PROC_ID %d: %d\n", proc_id, t);
+        // printf("total_procs %d\n", total_procs);
+
+        // Communicate accross processors
+        double recv_left, recv_right;
+        double send_left, send_right; 
+
+        if (total_procs <= 1) {
+            continue;
+        } else if (proc_id == 0) { // Leftmost processor case
+            printf("a\n");
+            send_right = proc_data[t][cols_per_proc - 1];
+            MPI_Sendrecv(&send_right, 1, MPI_DOUBLE, 1, 1,
+                          &recv_right, 1, MPI_DOUBLE, 1, 1, 
+                          MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            recv_left = proc_data[t][cols_per_proc - 2];
+            proc_data[t+1][cols_per_proc - 1] = proc_data[t][cols_per_proc - 1] + -k*(2*proc_data[t][cols_per_proc - 1] - recv_left - recv_right);
+    
+        } else if ((proc_id == (total_procs - 1))) { // Rightmost processor case
+            printf("b\n");
+            send_left = proc_data[t][0];
+            MPI_Sendrecv(&send_left, 1, MPI_DOUBLE, proc_id - 1, 1,
+                          &recv_left, 1, MPI_DOUBLE, proc_id - 1, 1, 
+                          MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            recv_right = proc_data[t][1];              
+            proc_data[t+1][0] = proc_data[t][0] + -k*(2*proc_data[t][0] - recv_left - recv_right);          
+            
+        } else { // Middle processor case 
+            // We need 2 MPI_Sendrecv one to the proc to the right and one to the left 
+            // sending my right most element to the recv_left because we are sending 
+            printf("c");
+            if (proc_id % 2 == 0) {
+                send_right = proc_data[t][cols_per_proc - 1];
+                MPI_Sendrecv(&send_right, 1, MPI_DOUBLE, proc_id + 1, 1, 
+                            &recv_right, 1, MPI_DOUBLE, proc_id + 1, 1,
+                            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                send_left = proc_data[t][0];               
+                MPI_Sendrecv(&send_left, 1, MPI_DOUBLE, proc_id - 1, 1,
+                            &recv_left, 1, MPI_DOUBLE, proc_id - 1, 1, 
+                            MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+            } else {
+                send_left = proc_data[t][0];               
+                MPI_Sendrecv(&send_left, 1, MPI_DOUBLE, proc_id - 1, 1,
+                            &recv_left, 1, MPI_DOUBLE, proc_id - 1, 1, 
+                            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                send_right = proc_data[t][cols_per_proc - 1];
+                MPI_Sendrecv(&send_right, 1, MPI_DOUBLE, proc_id + 1, 1, 
+                            &recv_right, 1, MPI_DOUBLE, proc_id + 1, 1,
+                            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+
+
+            // Then we just calculate the ends of our local array using the values we just recieved 
+            // calculating the end 
+            double left_diff = proc_data[t][0] - recv_left;  
+            double right_diff = proc_data[t][0] - proc_data[t][1];
+            double delta = -k*( left_diff + right_diff );
+            proc_data[t+1][0] = proc_data[t][0] + delta;
+
+            // now computing for the rightmost element of local array 
+            left_diff = proc_data[t][0] - proc_data[t][cols_per_proc - 2];
+            right_diff =  proc_data[t][0] - recv_right; 
+            delta = -k*( left_diff + right_diff );
+            proc_data[t+1][cols_per_proc - 1] = proc_data[t][cols_per_proc] + delta;
+        }
+
+    }
+
+    // Gather all of the results
+    MPI_Gather(proc_data, cols_per_proc, MPI_DOUBLE,
+                H, cols_per_proc, MPI_DOUBLE,
+                root_processor, MPI_COMM_WORLD);  // take another look at gather --> inappropiate use of H matrix most likely
+    // try rearrange H so it's a big block of matrix, readjust gather style; interleave the
+    // gather 0th row, 1st row; loop of gathers
+
+    // Print the results
+    if((proc_id == 0) && print){
+        // Column headers
+        printf("%3s| ","");
+        for(p=0; p<width; p++){
+            printf("%5d ",p);
+        }
+        printf("\n");
+        printf("%3s+-","---");
+        for(p=0; p<width; p++){
+            printf("------");
+        }
+        printf("\n");
+            // Row headers and data
+            for(t=0; t<max_time; t++){
+                printf("%3d| ",t);
+                for(p=0; p<width; p++){
+                printf("%5.1f ",H[t][p]); // check value of p
+                }
+                printf("\n");
+            }
+    }
+
+
+    // Deallocate proc_data
+    for(t=0; t<max_time; t++){
+        free(proc_data[t]);
+    }
+    free(proc_data);
+
+
+    // Clean up and deallocation
+    if (proc_id == 0){
+        for (int i = 0; i < max_time; i++){
+            // free(H[t]);
+        }
+        free(H);
+    }
+
+
+    MPI_Finalize();
+
+    return 0;
+}
