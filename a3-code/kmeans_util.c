9f3586ef583a7fead978ed9f3f8a0708291443b4
Jingli Kong <alpha282828@gmail.com>
2023-04-25 14:14:27 -0500

    starting cude portion

--- a3-code/kmeans_util.c
+++ a3-code/kmeans_util.c
@@ -10,6 +10,8 @@
 #include <stdint.h>
 #include <math.h>
 
+#include <limits.h>
+
 // Sets number of lines and total number of whitespace separated
 // tokens in the file. Returns -1 if file can't be opened, 0 on
 // success.
@@ -51,4 +53,153 @@ int filestats(char *filename, ssize_t *tot_tokens, ssize_t *tot_lines){
   // printf("DBG: tokens: %lu\n",ntokens);
   // printf("DBG: lines: %lu\n",nlines);
   return 0;
+}
+
+int intMax (int *arr, int len) {
+    int current_min = INT_MIN; 
+    for (int i = 0; i < len; i++) {
+        if (arr[i] > current_min) {
+            current_min = arr[i]; 
+        }
+    }
+    return current_min; 
+}
+typedef struct KMClust {
+    int nclust;   // number of clusters, the "k" in kmeans
+    int dim;      // dimension of features for data
+    float* features; // 2D indexing for individual cluster center features
+    int* counts;
+} KMClust;
+
+typedef struct KMData { // ndata is the number of features we have 
+    int ndata;    // count of data
+    int dim;      // dimension of features for data
+    float* features; // pointers to individual features also a 2D array storing all the elements for every feature
+    int* assigns; // cluster to which data is assigned
+    int* labels;  // label for data if available
+    int nlabels;  // max value of labels +1, number 0,1,...,nlabel0
+} KMData;
+
+KMData* kmdata_load(char* datafile) {
+    // KMData* data = malloc(sizeof(KMData));
+    // memset(data, 0, sizeof(KMData)); // zeroing out all data 
+    KMData* data = (KMData*) calloc(1, sizeof(KMData)); 
+    FILE* fin = fopen(datafile, "r");
+    if (fin == NULL) {
+        printf("Error opening file\n");
+        free(data);
+        return NULL;
+    }
+    ssize_t tot_tokens = 0; //number of tokens in datafile 
+    ssize_t tot_lines = 0;  // number of lines in datafile
+
+    // Getting file data we need to allocate correct amount of space 
+    int fileStats = filestats(datafile, &tot_tokens, &tot_lines); 
+ 
+    // allocating space for the number of labels in the dataset 
+    data->labels = (int *) calloc(tot_lines, sizeof(int)); 
+    //length of features array 
+    int featuresLength = tot_tokens/tot_lines - 2; 
+    // allocating space for all the features arrays 
+    data->features = (float *) malloc(tot_lines * featuresLength * sizeof(float)); // allocating a 2d array for the features 
+
+    int ndata = 0; // keeping track of ndata 
+    int currentIntToken = 0; // used to store the current feature token 
+    char colon[1]; 
+    for (int i = 0; i < tot_lines; i++) {
+        ndata++; 
+        fscanf (fin, "%d %s", &currentIntToken, colon);
+        data->labels[i] = currentIntToken; // appending label to labels array 
+        for (int j = 0; j < featuresLength; j++) {
+            fscanf(fin, "%d", &currentIntToken); // FIXME: Currently segfaulting here in test case 5  
+            data->features[i * featuresLength + j] = currentIntToken; // appending feature to feature array   
+        } // FIXME unsure as to why test case 4 is segfaulting 
+    }
+    fclose(fin);
+    data->ndata = ndata; 
+    data->dim = featuresLength; 
+    data->nlabels = intMax(data->labels, tot_lines) + 1; // note since I increment the labelIdx when I add a new label this should be the length
+    data->assigns = (int *) malloc(sizeof(int) * data->ndata); //allocating assigns array for later 
+    memset(data->assigns, 0, sizeof(int) * data->ndata); //zerioing out assigns for now 
+    
+    return data;
+}
+
+
+void helper_print(float* arr, int num_row, int num_col) {
+	// for (int i = 0; i < num_row; i++) {
+		for (int j = 0; j < num_col; j++) {
+			printf("%.0f ", arr[(0) * num_col + j]);
+		// }
+		// printf("------------------------------------------------------------------------------\n");
+	}
+  printf("\n------------------\n");
+  return;
+}
+
+
+KMClust* kmclust_new(int nclust, int dim) {
+    KMClust* clust = (KMClust*) malloc(sizeof(KMClust)); 
+    memset(clust, 0, sizeof(KMClust)); // zeroing out all data 
+    clust->nclust = nclust;
+    clust->dim = dim;
+
+    clust->features = (float *) malloc(sizeof(float) * nclust * dim); 
+    
+    clust->counts = (int *) malloc(sizeof(int) * nclust); 
+  
+    for (int c = 0; c < nclust; c++) {
+        for (int d = 0; d < dim; d++) {
+            clust->features[c * dim + d] = 0.0; 
+        }
+        clust->counts[c] = 0.0; 
+    }
+    return clust;
+}
+void save_pgm_files(KMClust* clust, char* savedir) {
+    int nclust = clust->nclust; 
+    int dim = clust->dim; 
+    int dim_root = (int) sqrt(dim); 
+    // if (clust->dim % dim_root == 0) {
+    if (clust->dim % dim_root == 0) {
+        printf("Saving cluster centers to %s/cent_0000.pgm ...\n", savedir); 
+        
+        float maxfeat = -INFINITY;  
+
+        for (int i = 0; i < nclust; i++) {
+            for (int j = 0; j < dim; j++) {
+                float element = clust->features[i * dim + j]; 
+                if (element > maxfeat) {
+                    maxfeat = element;  
+                }
+            }
+        }
+        for (int c = 0; c < nclust; c++) {
+            char outfile[1024]; 
+            sprintf(outfile, "%s/cent_%04d.pgm", savedir, c);
+            FILE *pgm = fopen(outfile, "w+"); 
+
+            fprintf(pgm,"P2\n");
+     
+            fprintf(pgm, "%d %d\n", dim_root, dim_root);
+            
+            fprintf(pgm,"%.0f", maxfeat);
+
+            for (int d = 0; d < dim; d++) {
+                if ((d > 0 && d%dim_root) == 0) {
+                    // fwrite("\n", 1, 1, pgm); 
+                    fprintf(pgm, "\n");
+                }
+				fprintf(pgm, "%3.0f ", clust->features[c*dim + d]); 
+                // int result = round(c * dim + d);
+                // fprintf(pgm, "%3d ", result);
+ 
+            }
+            // fwrite("\n", 1, 1, pgm); 
+            fprintf(pgm, "\n");
+            fclose(pgm);
+            
+        } 
+
+    }
 }
